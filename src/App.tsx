27ea9efb4855985d5f3e58073f68f4b4aebc76a0
almadomonkos06 <stieber.domonkos@gmail.com>
2025-09-05 13:39:21 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import './App.css';
 function App() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    //ez a comment petinek szól
     //i hate niggers i want to rape niggers
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const particlesInit = async (engine: any) => {
