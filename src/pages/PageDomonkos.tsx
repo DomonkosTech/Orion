10cc68bb70dc503498df7ad6bcae19202bb35ecf
almadomonkos06 <stieber.domonkos@gmail.com>
2025-09-26 13:11:18 +0200

    test

--- src/pages/PageDomonkos.tsx
+++ src/pages/PageDomonkos.tsx
@@ -5,7 +5,7 @@ function PageDomonkos() {
     // Score
     const [score, setScore] = useState(0);
 
-    // Upgrades asdasda asdasdasasdasdsa
+    // Upgrades asdasda asdasdasasdasdsadsadasask
     const [flatBonus, setFlatBonus] = useState(1);
     const [flatCost, setFlatCost] = useState(10);
 
