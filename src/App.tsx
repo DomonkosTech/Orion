7f6eb2061ad5982440a1f7fe0d1c26a6a7b61c58
peti-00 <petippg@gmail.com>
2025-10-28 11:29:00 +0100

    profile-editor.tsx létrehozása és össze csatolása az app.tsx-el

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx"; // Add this import
 import IsLoggedIn from "./IsLoggedIn.tsx";
+import Profile_editor from "./pages/profile-editor.tsx";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     </IsLoggedIn>
                 }
                 />
+                <Route path="/profile_editor" element={<Profile_editor />}/>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} /> {/* Add this route */}
             </Routes>
---
+++ src/pages/profile-editor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileEditor: React.FC = () => {
+    return (
+        <div>
+            {/* Profile editor content */}
+        </div>
+    );
+};
+
+export default ProfileEditor;
