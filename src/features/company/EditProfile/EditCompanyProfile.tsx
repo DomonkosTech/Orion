bc85ee83fcbea7526e209f0ac3618b3eaba06307
almadomonkos06 <stieber.domonkos@gmail.com>
2025-12-12 00:12:01 +0100

    EVERYTHING, basically everything

--- src/features/company/EditProfile/EditCompanyProfile.tsx
+++ src/features/company/EditProfile/EditCompanyProfile.tsx
@@ -30,7 +30,7 @@ const EditCompanyProfile: React.FC = () => {
             });
             const data = await res.json();
             if (data.success) {
-                try { window.dispatchEvent(new Event("auth-changed")); } catch {}
+                try { window.dispatchEvent(new Event("auth-changed")); } catch { /* empty */ }
                 alert("Sikeresen kijelentkeztél!");
                 navigate("/CompanyLoginPage");
             }
