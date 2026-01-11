8932f7876051d70d40e1e993eb5cac1067c370ff
peti-00 <petippg@gmail.com>
2026-01-11 17:38:54 +0100

    email aktiválás funkció refaktorálás

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,11 @@
 
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const supabaseUrl = "https://nlabffngmifszpwrqetx.supabase.co";
-const supabaseServiceKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sYWJmZm5nbWlmc3pwd3JxZXR4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTg2MDcyMywiZXhwIjoyMDc1NDM2NzIzfQ.q-Rhzv3VCnyiZKIhzNjFMyAjzgXCj8D8rJ7mVoTxgVg";
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
